905ff707b2ca86063141e1ad575d4e067034a5aa
GurkMuppen <henrik@waldeck.se>
2024-07-28 23:49:32 +0200

    Fixat trycksensor för att mäta höjd från start

--- bmp280test/bmp280test.ino
+++ bmp280test/bmp280test.ino
@@ -28,16 +28,11 @@ Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
-
-int ledCalPin = 6;
 float lokaltlufttryck = 1013.25;
-float calibr
-
-
-void nilsgillarbaguette(str printtext){
-  Serial.println(printtext);
-
-}
+float startAltitude = 0;
+float maxAltitude = 0;
+float upperReleaseThreshold = 1;
+//float calibr
 
 
 void setup() {
@@ -47,56 +42,63 @@ void setup() {
   unsigned status;
   //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
 
-  
-  pinMode(ledCalPin, OUTPUT);
-
   status = bmp.begin();
+
   if (!status) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
                       "try a different address!"));
     Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
     while (1) delay(10);
   }
 
-
-
-
-
-  /* Default settings from datasheet. */
+  /* SET PRESSURE SAMPLING*/
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+                  Adafruit_BMP280::SAMPLING_X1,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X2,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_125); /* Standby time. */
 
-  int calibrationTests = 0;
 
   //calibrate 
-  digitalWrite(ledCalPin, HIGH);
-  for (i = 0; i >= 10; i++;){
-    calibrationTests += bmp.readAltitude(lokaltlufttryck)
+  float calibrationHeight = 0;
+
+  for (int i = 0; i < 10; i++){
+    calibrationHeight += bmp.readAltitude(lokaltlufttryck);
+    delay(150);
   }
-  
+
+  startAltitude = calibrationHeight / 10;
 
 }
 
 void loop() {
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
 
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
 
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(lokaltlufttryck)); /* Adjusted to local forecast! */
-    Serial.println(" m");
+  float altitude = bmp.readAltitude(lokaltlufttryck) - startAltitude;
 
-    Serial.println();
-    delay(2000);
+  Serial.print(F("Approx altitude = "));
+  Serial.print(altitude); /* Adjusted to local forecast! */
+  Serial.println(" m");
+
+
+  Serial.println();
+
+  // ALTITUDE RELEASE SYSTEM
+  if (maxAltitude < altitude)
+  {
+    maxAltitude = altitude;
+  }
+
+  if (maxAltitude - altitude > upperReleaseThreshold)
+  {
+    Serial.println("RELEASE CHUTE");
+  }
+
+  delay(1000);
 }
